4d065a123fb92f7ee06e02dae1a7517b9e5d8f12
GauravLamichhane <gaurav.lamichhane02@gmail.com>
2025-07-10 20:07:17 +0545

    prepare for railways

--- src/saashome/settings.py
+++ src/saashome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = "django-insecure-b2405o!@@^@hp+zj7u3%p@k#vdjd&c88%w@l@1ki7!xmno-799
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  ".railway.app"#https://saas.prod.railway.app
+]
+if DEBUG:
+  ALLOWED_HOSTS += [
+    "127.0.0.1",
+    "localhost"
+  ]
 
 
 # Application definition
--- src/saashome/views.py
+++ src/saashome/views.py
@@ -1,20 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from visits.models import PageVisit
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+  return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
   qs = PageVisit.objects.all()
   page_qs = PageVisit.objects.filter(path = request.path)
+  try:
+    percent = (page_qs.count() * 100.0) / qs.count()
+  except:
+    percent = 0
   title = "My Page"
   context = {
     'page_title':title,
     'page_visit_count':page_qs.count(),
     'total_visit_count':qs.count(),
-  }
+    'percent':percent  }
 
   path = request.path
   print("path", path)
-  PageVisit.objects.create(path=request.path)#This line creates a new row in the DB like path = '/contact/' timestamp = 2025-0-0
-
+  PageVisit.objects.create(path=request.path)
   return render(request,'home.html',context)
+
 
 #1:11:18
